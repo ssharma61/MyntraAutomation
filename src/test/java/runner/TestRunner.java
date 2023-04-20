50a32d1607c3d2bc7abdd7debae08fe5ec7e1b3f
swati.sharma <swati.sharma@geminisolutions.com>
2023-04-20 19:03:49 +0530

    sixth commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,7 +1,5 @@
 package runner;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
